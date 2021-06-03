255cca41f1c2a7940b709f8efb2e763c9e97e1a8
sebastian <juans.escobarg@konradlorenz.edu.co>
2021-06-03 11:54:42 -0500

    agreugue botones de cambio de color, falta trabajar un poco en ellos

---
+++ js/botones.js
@@ -0,0 +1,11 @@
+function noche(){
+    if(document.getElementById("noche").checked){
+        document.body.style.background='#000000';
+    }
+}
+
+function dia(){
+    if(document.getElementById("dia").checked){
+        document.body.style.background='#ffffff';
+    }
+}
