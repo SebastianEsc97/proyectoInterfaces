14f968285da9ee55fd94e416236ca15f25edfcd4
sebastian <juans.escobarg@konradlorenz.edu.co>
2021-06-03 16:15:50 -0500

    Agregue botones y acomode algunas cosas de las navbar

--- js/botones.js
+++ js/botones.js
@@ -2,6 +2,7 @@ function noche() {
     document.body.className = "btn-noche";
     document.querySelector("#dia").style.color = "grey";
     document.querySelector("#navbar1").style.background = "#000";
+    document.querySelector("#navbar3").style.background = "#000";
     document.querySelector("#textUser").style.color = "#fff";
 }
 
@@ -9,16 +10,16 @@ function noche() {
 function dia() {
     document.body.className = "btn-dia";
     document.querySelector("#navbar1").style.background = "#fff";
+    document.querySelector("#navbar3").style.background = "#fff";
     document.querySelector("#textUser").style.color = "#000";
-    document.querySelector("#dia").style.color = "grey";
 }
 
 function titulocolor1(){
     document.body.className = "titulocolor-1";
-    document.querySelector(".titulocolor-1").style.color = "green";
+    document.querySelector("#titulocolor-1").style.color = "black";
 }
 
 function titulocolor2(){
-    document.body.className = "titulocolor-1";
-    document.querySelector("titulocolor-1").style.color = "green";
+    document.body.className = "titulocolor-2";
+    document.querySelector("#titulocolor-2").style.color = "red";
 }
